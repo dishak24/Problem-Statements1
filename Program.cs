9e3d1ad97b910ad8925e9e60819bd54c2402cca4
Disha <dishakamble24@gmail.com>
2025-02-06 13:41:59 +0530

    Main Method

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProblemStatement1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to the Problem Statement Series of C#.");
+        }
+    }
+}
